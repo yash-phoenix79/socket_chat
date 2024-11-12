7c9ccab83253cb8d629b2034cdc3515efcc7c305
Yaswanth <71338071+yash-phoenix79@users.noreply.github.com>
2024-11-12 18:32:55 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,105 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
+import { dirname, join } from 'node:path';
+import { Server } from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
+
+// open the database file
+const db = await open({
+  filename: 'chat.db',
+  driver: sqlite3.Database
+});
+
+// create our 'messages' table (you can ignore the 'client_offset' column for now)
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server,{
+    connectionStateRecovery: {}
+  });
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.get('/', (req, res) => {
+    res.sendFile(join(__dirname, 'index.html'));
+});
+
+
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    // create one worker per available core
+    for (let i = 0; i < numCPUs; i++) {
+      cluster.fork({
+        PORT: 3010 + i
+      });
+    }
+    
+    // set up the adapter on the primary thread
+    setupPrimary();
+  } else {
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+      connectionStateRecovery: {},
+      // set up the adapter on each worker thread
+      adapter: createAdapter()
+    });
+     // [...]
+
+  // each worker will listen on a distinct port
+  const port = process.env.PORT;
+
+  server.listen(port, () => {
+    console.log(`server running at http://localhost:${port}`);
+  });
+}
+
+io.on('connection', async(socket) => {
+    console.log('a user connected');
+    socket.on('chat message', async(msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+        let result;
+        try {
+          // store the message in the database
+          result = await db.run('INSERT INTO messages (content) VALUES (?)', msg);
+        } catch (e) {
+          // TODO handle the failure
+          return;
+        }
+        // include the offset with the message
+        io.emit('chat message', msg, result.lastID);
+    });
+    if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+          await db.each('SELECT id, content FROM messages WHERE id > ?',
+            [socket.handshake.auth.serverOffset || 0],
+            (_err, row) => {
+              socket.emit('chat message', row.content, row.id);
+            }
+          )
+        } catch (e) {
+          // something went wrong
+        }
+      }
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+server.listen(3010, () => {
+    console.log('server running at http://localhost:3010');
+});
